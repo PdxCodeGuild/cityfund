1898983598c0a2624bb01514ffade30ab8466f2e
KBratland <kristin.bratland@gmail.com>
2014-12-09 16:09:35 -0800

    integrated front and back end in django

---
+++ city_fund/signup/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class SignUp(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    message = models.TextField(max_length=1000, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.email
