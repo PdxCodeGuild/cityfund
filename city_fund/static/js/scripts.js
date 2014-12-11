4f2ebfb394cf036a6f4f0bccc5968e45536b6849
iamjasn <jkuhafkan@gmail.com>
2014-12-11 00:00:46 -0800

    add scroll to id click event

---
+++ city_fund/static/js/scripts.js
@@ -0,0 +1,17 @@
+$().ready(function() {
+
+    var scrollToId = function(id) {
+        var element = $("#" + id);
+        $('html, body').animate({
+            scrollTop: element.offset().top
+                }, 500 );
+    }
+
+    $("a").click(function(event) {
+        if ($(this).attr("href").match("#")) {
+            event.preventDefault();
+            var pageSection = $(this).attr('href').replace('#', '')
+            scrollToId(pageSection);
+            }
+    });    
+});
